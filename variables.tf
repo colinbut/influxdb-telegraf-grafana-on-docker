2221212c29a72869e4b560c35c40f88e6756c217
Colin But <colin.but@outlook.com>
2020-12-06 22:31:40 +0000

    chore: terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "key_name_pair" {
-  type = string
+  type        = string
   description = "The name of the Key Pair to ssh into the machine"
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "The type of the EC2 instance to create"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
