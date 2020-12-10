47705e312926b1d0d39cfac4cbff765582468c16
Colin But <colin.but@outlook.com>
2020-12-10 20:45:00 +0000

    feat: put in security groups

--- main.tf
+++ main.tf
@@ -38,3 +38,38 @@ resource "aws_instance" "host_server" {
     "Name" = "InfluxDB_Telegraf_Host_Server"
   }
 }
+
+resource "aws_security_group" "security_group" {
+  name = "host_server_sg"
+  description = "The SG that allows both ssh & access to grafana via the web"
+}
+
+resource "aws_security_group_rule" "allow_access_to_grafana" {
+    type                = "ingress"
+    description         = "allowing access to Grafana on default port"
+    from_port           = 3000
+    to_port             = 3000
+    protocol            = "tcp"
+    cidr_blocks         = ["0.0.0.0/0"] # for demo purposes i'm allowing from every one
+    security_group_id   = aws_security_group.security_group.id
+}
+
+resource "aws_security_group_rule" "allow_ssh_from_bastian" {
+    type                = "ingress"
+    description         = "allow ssh from outside"
+    from_port           = 22
+    to_port             = 22
+    protocol            = "tcp"
+    cidr_blocks         = ["0.0.0.0/0"] # for demo purposes i'm allowing from every one
+    security_group_id   = aws_security_group.security_group.id
+}
+
+resource "aws_security_group_rule" "default_outbound" {
+    type                = "egress"
+    description         = "default_outbound"
+    from_port           = 0
+    to_port             = 0
+    protocol            = "-1"
+    cidr_blocks         = ["0.0.0.0/0"]
+    security_group_id   = aws_security_group.security_group.id
+}
